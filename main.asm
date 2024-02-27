025729ed49d7870b8360c429aecd51d4982cf0b6
Andrey Shalaev <65027666+Hukyl@users.noreply.github.com>
2024-02-28 01:44:26 +0200

    feature: read code procedure

--- main.asm
+++ main.asm
@@ -3,38 +3,43 @@
 ; Agreement on code comments:
 ;   [8] refers to Tom Swan's book "Mastering Turbo Assmbler, 2nd edition"
 
-TAIL_BYTES EQU 80h
-TAIL_START EQU 81h
+
+;;;;;;;;;;;;;;;;;;;;;;; Constants ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+CODE_SIZE   EQU 10000
+CELLS_SIZE  EQU 10000
+
+TAIL_BYTES  EQU 80h
+TAIL_START  EQU 81h
 TAIL_LENGTH EQU 127
+
+CR          EQU 13
+LF          EQU 10
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 .data?
     ; Define variables
     ;   Declaring uninitialized variables decrease the file size
     ;   [8], page 25
-    filename  db TAIL_LENGTH DUP(?)
-    code      db 10000 DUP(?)
-    cells     db 10000 DUP(?)
+    filename  db TAIL_LENGTH    DUP(?)
+    code      db CODE_SIZE      DUP(?)
+    cells     dw CELLS_SIZE     DUP(?)
     ; TODO: investigate `mov cx, 10000` -> `cld` -> `rep stosb`
     ; Book: [8], page: 137
+
 
 .code
 org 100h
 
 main PROC
-    mov ax, cs
-    mov es, ax
-
-    xor   bx, bx
-    mov   bl, [0h:TAIL_BYTES]
-    mov   al, '$'
-    mov   byte [bx+TAIL_START], al
+    mov ax, cs      ; IMPORTANT: do not change `ds` before `readCommandTail`
+    mov es, ax      ; Since variables are stored in the code segment
     
     call readCommandTail
 
-    xor   ax, ax
-    mov   ah, 9
-    mov   dx, offset filename
-    int   21h
+    mov ds, ax
+
+    call readCode
 
     ; Possible replace to `int 20h`
     ; mov ax, 4C00h  ; Close and flish all open file handles.
@@ -51,12 +56,39 @@ readCommandTail PROC
     ; Output: 
     ;   [filename] string    
     cld                         ; clear direction (flag)
-    mov si, TAIL_START          ; read from the tail start
+    mov si, TAIL_START+1        ; read from tail start (ignore leading whitespace)
     mov di, offset filename     ; write to variable
-    mov cx, TAIL_LENGTH         ; set counter to length of tail
+    mov cl, byte [si-3]         ; number of bytes to read (ignore ^Z and whitespace char)
+    dec cx
     rep movsb
     ret
 readCommandTail ENDP
+
+
+readCode PROC
+    ; Input: 
+    ;   [filename] string
+    ; Registers used:
+    ;   ax, dx, bx, cx
+    ; Output: 
+    ;   
+    lea dx, filename
+    mov ah, 03Dh        ; Open file DOS function
+    mov al, 0           ; Read-only mode
+    int 21h
+
+    ; No error checking, since is guaranteed by requirements
+    mov bx, ax          ; File handle
+    mov ah, 3Fh         ; Read file DOS function
+    mov cx, CODE_SIZE   ; Number of bytes to read
+    lea dx, code        ; Where to store the code
+    int 21h
+
+    mov ah, 3Eh         ; Close file DOS function
+    ; bx is preset to file handle
+    int 21h
+    ret
+readCode ENDP
 
 
 decodeCommand PROC NEAR
