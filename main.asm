e29ebee0fc44e584c591c565827bd4346c1246e3
Andrey Shalaev <65027666+Hukyl@users.noreply.github.com>
2024-02-29 02:35:51 +0200

    feature: partially implement command decoding and executing

--- main.asm
+++ main.asm
@@ -21,9 +21,10 @@ LF          EQU 10
     ; Define variables
     ;   Declaring uninitialized variables decrease the file size
     ;   [8], page 25
-    filename  db TAIL_LENGTH    DUP(?)
-    code      db CODE_SIZE      DUP(?)
-    cells     dw CELLS_SIZE     DUP(?)
+    filename    db TAIL_LENGTH    DUP(?)
+    code        db CODE_SIZE      DUP(?)
+    cells       dw CELLS_SIZE     DUP(?)
+    loopCounter dw 0
     ; TODO: investigate `mov cx, 10000` -> `cld` -> `rep stosb`
     ; Book: [8], page: 137
 
@@ -34,13 +35,19 @@ org 100h
 main PROC
     mov ax, cs      ; IMPORTANT: do not change `ds` before `readCommandTail`
     mov es, ax      ; Since variables are stored in the code segment
-    
     call readCommandTail
 
     mov ds, ax
-
     call readCode
 
+    xor cx, cx
+    mov si, offset code
+    mov di, offset cells
+decodeLoop:
+    lodsb   ; al <- [ds:si], si <- si + 1
+    call decodeCommand
+    cmp al, 0
+    jne decodeLoop
     ; Possible replace to `int 20h`
     ; mov ax, 4C00h  ; Close and flish all open file handles.
     ; int 21h
@@ -72,16 +79,16 @@ readCode PROC
     ;   ax, dx, bx, cx
     ; Output: 
     ;   [code] ASCIIZ string
-    lea dx, filename
+    mov dx, offset filename
     mov ah, 03Dh        ; Open file DOS function
     mov al, 0           ; Read-only mode
     int 21h
 
     ; No error checking, since is guaranteed by requirements
-    mov bx, ax          ; File handle
-    mov ah, 3Fh         ; Read file DOS function
-    mov cx, CODE_SIZE   ; Number of bytes to read
-    lea dx, code        ; Where to store the code
+    mov bx, ax              ; File handle
+    mov ah, 3Fh             ; Read file DOS function
+    mov cx, CODE_SIZE       ; Number of bytes to read
+    mov dx, offset code     ; Where to store the code
     int 21h
 
     mov ah, 3Eh         ; Close file DOS function
@@ -93,63 +100,138 @@ readCode ENDP
 
 decodeCommand PROC NEAR
     ; Input:
-    ;   [code] ASCIIZ string
+    ;   [loopCount] - number of loops
+    ;   si - command address
+    ;   di - cell address
     ; Registers used:
-    ;   ax, bx - temporary registers
-    ;   cx - command index
-    ;   dx - cell index
-    ;   si, di - string I/O
+    ;   see used procedures
     ; Output:
     ;   None
-    mov cl, code
-    mov dx, cells
 
+    cmp al, '>'
+    je incrementPointer
+    cmp al, '<'
+    je decrementPointer
+    cmp al, '+'
+    je incrementValue
+    cmp al, '-'
+    je decrementValue
+    cmp al, '.'
+    je writeLabel
+    cmp al, ','
+    je readLabel
+    cmp al, '['
+    je start_loop
+    cmp al, ']'
+    je end_loop
+    jmp exitDecode
+incrementPointer:
+    add di, 2
+    jmp exitDecode
+decrementPointer:
+    sub di, 2
+    jmp exitDecode
+incrementValue:
+    inc word ptr [di]
+    jmp exitDecode
+decrementValue:
+    dec word ptr [di]
+    jmp exitDecode
+writeLabel:
+    call writeChar
+    jmp exitDecode
+readLabel:
+    call readChar
+    jmp exitDecode
+start_loop:
+    inc [loopCounter]
+    jmp exitDecode
+end_loop:
+    call endLoop
+exitDecode:
+    ret
 decodeCommand ENDP
 
 
-increment PROC NEAR
-    xor ax, ax
-increment ENDP
-
-
-decrement PROC NEAR
-    xor ax, ax
-decrement ENDP
-
-
-incrementPointer PROC NEAR
-    xor ax, ax
-incrementPointer ENDP
-
-
-decrementPointer PROC NEAR
-    xor ax, ax
-decrementPointer ENDP
-
-
-startLoop PROC NEAR
-    xor ax, ax
-startLoop ENDP
-
-
 endLoop PROC NEAR
-    xor ax, ax
+    ; endLoop tries to finish the loop, or jump to the beginning if counter != 0.
+    ; Upon seeing inner loops:
+    ;   1. Check if [loopCounter] == starting loop count
+    ;   2. If not, continue moving to the left.
+    ;   3. Else, finish
+    ; Input:
+    ;   di - cell address
+    ;   si - command address
+    ;   [loopCounter] - current loop count
+    ; Registers used:
+    ;   bx - storing starting loop count
+    ; Output:
+    ;   None
+
+    cmp word ptr [di], 0
+    je finishLoop
+    dec word ptr [di]
+    sub si, 2               ; As si points at char after ]
+    mov bx, [loopCounter]   ; Storing counter to account for inner loops
+    std                     ; Move si backwards on lodsb
+findOpeningBracket:
+    lodsb
+    cmp al, ']'
+    je metInnerLoop
+    cmp al, '['
+    jne findOpeningBracket
+    ; If loopCounter == bx, we reached the start of required loop
+    ; Else continue searching
+    cmp bx, [loopCounter]
+    je resetSI
+    dec [loopCounter]
+    jmp findOpeningBracket
+metInnerLoop:
+    ; If we met ending of an inner loop, we have to account for
+    ; it by incrementing loopCounter
+    inc [loopCounter]
+    jmp findOpeningBracket
+resetSI:
+    cld         ; Move si forward on lodsb
+    add si, 2   ; As si points at char before [
+    jmp exitLoopProc
+finishLoop:
+    dec [loopCounter]
+exitLoopProc:
+    ret
 endLoop ENDP
 
 
 writeChar PROC NEAR
-    xor ax, ax
+    ; Input:
+    ;   di - cell address
+    ;   dx - cell address 
+    ; Registers used:
+    ;   ax, bx, si
+    ; Output:
+    ;   None
+    mov ah, 40h     ; Write to file DOS function
+    mov bx, 1       ; Stdout
+    mov cx, 1       ; 1 bytes
+    mov dx, di
+    int 21h
+    ret
 writeChar ENDP
 
 
 readChar PROC NEAR
-    xor ax, ax
+    ; Input:
+    ;   di - cell address 
+    ; Registers used:
+    ;   ax, bx, si
+    ; Output:
+    ;   None
+    ; TODO: DRY
+    mov ah, 3Fh     ; Read from file DOS function        
+    mov bx, 0       ; Stdin
+    mov cx, 1       ; 1 bytes
+    int 21h
+    ret
 readChar ENDP
-
-
-exit PROC NEAR
-    xor ax, ax
-exit ENDP
-
 
 END main
